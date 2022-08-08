6ad8813e7e8b093ca51a732e0130496f956069d3
pierrek6e <pierrecass75@gmail.com>
2022-08-08 14:38:22 +0700

    set first api call

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,32 @@
-import type { NextPage } from "next"
+import axios from "axios"
+import NoResult from "../components/NoResult"
+import VideoCard from "../components/VideoCard"
+import { Video } from "../types"
 
-const Home: NextPage = () => {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+interface IProps {
+  videos: Video[]
+}
+
+const Home = ({ videos }: IProps) => {
+  return (
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length? videos.map((video: Video)=>(
+        <VideoCard post={video} key={video._id}/>
+      ) )
+    : (
+      <NoResult text='No Videos' />
+    )
+    }
+      
+    </div>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get(`http://localhost:3000/api/post`)
+
+  console.log(data)
+  return { props: { videos: data } }
 }
 
 export default Home
---
+++ utils/index.ts
@@ -0,0 +1,4 @@
+import axios from 'axios';
+
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
